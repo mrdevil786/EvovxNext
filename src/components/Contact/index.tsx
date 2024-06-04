3fe29f08910cce5bf87c43c006b3782c327a1cbc
mrdevil786 <suhailsaeedme@gmail.com>
2024-06-04 23:09:01 +0530

    Initial Commit

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
       <div className="container">
         <div className="-mx-4 flex flex-wrap">
-          <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
+          <div className="w-full px-4">
             <div
               className="mb-12 rounded-sm bg-white px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
               data-wow-delay=".15s
@@ -52,6 +52,21 @@ const Contact = () => {
                   <div className="w-full px-4">
                     <div className="mb-8">
                       <label
+                        htmlFor="phone"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Your Phone
+                      </label>
+                      <input
+                        type="phone"
+                        placeholder="Enter your phone"
+                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4">
+                    <div className="mb-8">
+                      <label
                         htmlFor="message"
                         className="mb-3 block text-sm font-medium text-dark dark:text-white"
                       >
@@ -65,7 +80,7 @@ const Contact = () => {
                       ></textarea>
                     </div>
                   </div>
-                  <div className="w-full px-4">
+                  <div className="w-full px-4 text-center">
                     <button className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
                       Submit Ticket
                     </button>
@@ -74,9 +89,9 @@ const Contact = () => {
               </form>
             </div>
           </div>
-          <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
+          {/* <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
             <NewsLatterBox />
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
